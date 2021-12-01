ba0da3bf9b2b31a4f18ade796a4116eba31acdcf
msskk15 <94050473+msskk15@users.noreply.github.com>
2021-12-01 16:59:11 +0530

    Updated Checkout page

--- CheckoutPage.java
+++ CheckoutPage.java
@@ -14,52 +14,57 @@ public class CheckoutPage {
     @FindBy(name="order[bill_address_attributes][lastname]")
     WebElement lastname;
 
-    @FindBy(name="order[bill_address_attributes][address1]")
+    @FindBy(css="input[id='order_bill_address_attributes_address1']")
     WebElement address1;
 
+    @FindBy (css ="input[id='order_bill_address_attributes_address2']")
+    WebElement address2;
+
     @FindBy(id="order_bill_address_attributes_city")
-    WebElement city;
+    WebElement cityName;
 
     @FindBy(id="order_bill_address_attributes_state_id")
     WebElement state;
 
     @FindBy(name="order[bill_address_attributes][zipcode]")
-    WebElement zipcode;
+    WebElement zipCodeNumber;
 
     @FindBy(id="order_bill_address_attributes_country_id")
     WebElement country;
 
     @FindBy(id="order_bill_address_attributes_phone")
-    WebElement phone;
+    WebElement phoneNumber;
 
     @FindBy(id="save_user_address")
     WebElement unchkSaveAddress;
 
-    @FindBy(name="commit")
-    WebElement save;
+    @FindBy(id="order_use_billing")
+    WebElement chkBillingAddress;
+
+    @FindBy (css = "input[data-disable-with='Save and Continue']")
+    WebElement SaveAndContinue;
+
+   // @FindBy(name="commit")
+    //WebElement save;
 
     public CheckoutPage(WebDriver driver)
     {
         PageFactory.initElements(driver,this);
     }
-    public void addBillingAddress(String firstname, String lastname, String address, String city,String state,String zipcode,String country, String phoneno)
-    {
-        this.firstname.sendKeys(firstname);
-        this.lastname.sendKeys(lastname);
-        this.address1.sendKeys(address);
-        this.city.sendKeys(city);
+    public void addBillingAddress(String fname , String lname , String add1 , String add2 ,
+                                  String city, String zipCode, String phoneNum){
+        firstname.sendKeys(fname);
+        lastname.sendKeys(lname);
+        address1.sendKeys(add1);
+        address2.sendKeys(add2);
+        cityName.sendKeys(city);
+        country.click();
+        country.sendKeys("California");
+        zipCodeNumber.sendKeys(zipCode);
+        phoneNumber.sendKeys(phoneNum);
+        SaveAndContinue.click();
 
-        Select st=new Select(this.state);
-        st.selectByVisibleText(state);
 
-        this.zipcode.sendKeys(zipcode);
-
-        Select cntry=new Select(this.country);
-        cntry.selectByVisibleText(country);
-
-        phone.sendKeys(phoneno);
-        unchkSaveAddress.click();
-        save.click();
     }
 }
 
